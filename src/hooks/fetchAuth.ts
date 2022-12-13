c8e1d0525167ab8481b062d763f841b0ca229197
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-13 15:55:35 +0900

    create fetchAsyncUpdateProf

--- src/hooks/fetchAuth.ts
+++ src/hooks/fetchAuth.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
-import { PROPS_AUTHEN, PROPS_NICKNAME } from '../features/types'
+import { PROPS_AUTHEN, PROPS_NICKNAME, PROPS_PROFILE } from '../features/types'
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL as string
 
@@ -39,6 +39,27 @@ export const fetchAuth = () => {
           Authorization: `JWT ${localStorage.localJWT as string}`,
         },
       })
+      return res.data
+    }
+  )
+
+  const fetchAsyncUpdateProf = createAsyncThunk(
+    'profile/update',
+    async (profile: PROPS_PROFILE) => {
+      const uploadData = new FormData()
+      uploadData.append('nickName', profile.nickName)
+      if (profile.img !== null)
+        uploadData.append('img', profile.img, profile.img.name)
+      const res = await axios.put(
+        `${apiUrl}api/profil/${profile.id}`,
+        uploadData,
+        {
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: `JWT ${localStorage.localJWT as string}`,
+          },
+        }
+      )
       return res.data
     }
   )
