2936c798d8e277f763343eb871d4028dbaf11aab
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-13 16:19:17 +0900

    delete counterSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
-import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
   },
 })
