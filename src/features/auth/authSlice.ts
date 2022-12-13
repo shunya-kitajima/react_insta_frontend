4e0d1f0ac03cc17cc7f4a64dc75872be140af0e3
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-13 15:16:34 +0900

    delete waste and set initialState

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,1 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { RootState } from '../../app/store'
+import { File, PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from '../types'
+
+const apiUrl = process.env.REACT_APP_DEV_API_URL
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    openSignIn: true,
+    openSignUp: false,
+    openProfile: false,
+    isLoadingAuth: false,
+    myprofile: {
+      id: '',
+      nickName: '',
+      userProfile: '',
+      img: '',
+      created_at: '',
+    },
+    profiles: [
+      {
+        id: '',
+        nickName: '',
+        userProfile: '',
+        img: '',
+        created_at: '',
+      },
+    ],
+  },
+  reducers: {},
+  extraReducers: (builder) => {},
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export const selectCount = (state: RootState) => state.counter.value
+
+export default authSlice.reducer
