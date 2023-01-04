87cfb1057c564e2bf9a275802d17b8f54c0e3c8c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-04 16:39:30 +0900

    display Auth component

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Auth from '../auth/Auth'
 
 const Core: React.FC = () => {
-  return <div></div>
+  return (
+    <div>
+      <Auth />
+    </div>
+  )
 }
 
 export default Core
