1ea6c5ec16f50604142490fd5667d1b4a36526d7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-09 15:00:21 +0900

    display post and modify name

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -16,7 +16,7 @@ import { selectProfiles } from '../auth/authSlice'
 import { fetchPost } from '../../hooks/fetchPost'
 import { fetchPostStart, fetchPostEnd, selectComments } from './postSlice'
 import { PROPS_POST } from '../types'
-import styles from './Post.modlule.css'
+import styles from './Post.module.css'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -42,7 +42,7 @@ const Post: React.FC<PROPS_POST> = ({
   const profiles = useSelector(selectProfiles)
   const comments = useSelector(selectComments)
   const commentsOnPost = comments.filter((comment) => (comment.post = postId))
-  const profiel = profiles.filter((prof) => prof.userProfile === userPost)
+  const profile = profiles.filter((prof) => prof.userProfile === userPost)
   const { fetchAsyncPostComment, fetchAsyncPatchLiked } = fetchPost()
 
   const postCommentHandler = async (
@@ -68,7 +68,38 @@ const Post: React.FC<PROPS_POST> = ({
     dispatch(fetchPostEnd)
   }
 
-  return <div></div>
+  if (title !== '') {
+    return (
+      <div className={styles.post}>
+        <div className={styles.post_header}>
+          <Avatar className={styles.post_avatar} src={profile[0]?.img} />
+          <h3>{profile[0]?.nickName}</h3>
+        </div>
+        <img className={styles.post_image} src={imageUrl} alt="" />
+        <h4 className={styles.post_text}>
+          <Checkbox
+            className={styles.post_checkBox}
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite />}
+            checked={liked.some((like) => like === loginId)}
+            onChange={patchLikedHandler}
+          />
+          <strong>{profile[0]?.nickName}</strong>
+          {title}
+          <AvatarGroup max={7}>
+            {liked.map((like) => (
+              <Avatar
+                className={styles.post_avatarGroup}
+                key={like}
+                src={profiles.find((prof) => prof.userProfile === like)?.img}
+              />
+            ))}
+          </AvatarGroup>
+        </h4>
+      </div>
+    )
+  }
+  return null
 }
 
 export default Post
