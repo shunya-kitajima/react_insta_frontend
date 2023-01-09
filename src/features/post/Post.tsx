3b5fc651b98679dfa9ea5a82b6176147587787d6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-09 14:29:06 +0900

    set useStyles

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { AppDispatch } from '../../app/store'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core'
 import { Avatar, Divider, Checkbox } from '@material-ui/core'
 import { Favorite, FavoriteBorder } from '@material-ui/icons'
 import AvatarGroup from '@material-ui/lab/AvatarGroup'
@@ -10,6 +10,16 @@ import { fetchPost } from '../../hooks/fetchPost'
 import { fetchPostStart, fetchPostEnd, selectComments } from './postSlice'
 import { PROPS_POST } from '../types'
 import styles from './Post.modlule.css'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+      marginRight: theme.spacing(1),
+    },
+  })
+)
 
 const Post: React.FC<PROPS_POST> = ({
   postId,
