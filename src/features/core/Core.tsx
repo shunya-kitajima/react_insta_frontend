2405fb0aa89470a6f9747c60f0f516ab0cd8ffd0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-09 10:39:00 +0900

    import modules

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { AppDispatch } from '../../app/store'
 import Auth from '../auth/Auth'
-import { styles } from './Core.module.css'
+import styles from './Core.module.css'
 
 const Core: React.FC = () => {
   return (
