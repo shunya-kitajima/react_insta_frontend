0faf780b3c136e5dfe557345ed1128aa3d3a1a33
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-10 10:35:54 +0900

    import modules and create handlers

---
+++ src/features/core/NewPost.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { AppDispatch } from '../../app/store'
+import Modal from 'react-modal'
+import { Button, TextField, IconButton } from '@material-ui/core'
+import { MdAddAPhoto } from 'react-icons/md'
+import { fetchPost } from '../../hooks/fetchPost'
+import {
+  fetchPostStart,
+  fetchPostEnd,
+  selectOpenNewPost,
+  resetOpenNewPost,
+} from '../post/postSlice'
+import { File } from '../types'
+import styles from './Core.module.css'
+
+const customStyles = {
+  content: {
+    top: '55%',
+    left: '50%',
+    width: 280,
+    height: 220,
+    padding: '50px',
+    transform: 'translate(-50%, -50%)',
+  },
+}
+
+const NewPost: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch()
+  const { fetchAsyncNewPost } = fetchPost()
+  const openNewPost = useSelector(selectOpenNewPost)
+  const [image, setImage] = useState<File | null>(null)
+  const [title, setTitle] = useState('')
+
+  const newPost = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault()
+    const packet = { title, img: image }
+    dispatch(fetchPostStart())
+    await dispatch(fetchAsyncNewPost(packet))
+    dispatch(fetchPostEnd())
+    setImage(null)
+    setTitle('')
+    dispatch(resetOpenNewPost())
+  }
+
+  const editPictureHandler = (): void => {
+    const fileInput = document.getElementById('imageInput')
+    fileInput?.click()
+  }
+
+  return <div>NewPost</div>
+}
+
+export default NewPost
